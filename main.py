7d1cc839b744ca72cfc7150e8845e0f75d70a432
Albert Mosella <albert.mosella@gmail.com>
2013-12-07 15:22:48 +0100

    translate

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import os
+import glob as g
+import MySQLdb as sql
+import aprenentatge
+import learndatamani as l
+
+path = './image/*.jpg' 
+tam = len(path) - 5  
+x = g.glob(path)  
+nfile = raw_input("Introdueix el nom del fitxer del aprenentatge:") 
+if not os.path.isfile(nfile): 
+	query = "SELECT count(*) FROM sed2013_task2_dataset_train_tags;" 
+	db = sql.connect(host="localhost", user="root", passwd="root",db="gdsa") 
+	learn = aprenentatge.aprenentatge(x,tam,db)
+  	l.serialization(learn,nfile)
+else : 
+	learn= l.deserialization(nfile)
+   
+ 
+path = raw_input( "Introdueix el path de la carpeta de imatges a classificar, path relatiu o absolut \n")
+tam = len(path)
+x = g.glob(path+"/*.jpg")
+for image in x:
+	image = image[tam:-4]
+	#clasificació de la imatge
+   	clasificador(learn,image,db)
+db.close()
