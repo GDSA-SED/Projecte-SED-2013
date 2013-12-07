99d7ca25cf750192a4154f69fdd824cc7a8943c4
Albert Mosella <albert.mosella@gmail.com>
2013-12-07 09:55:56 +0100

    Afegit la serialització i la deserialització del mapren

--- esquelet.py
+++ esquelet.py
@@ -5,24 +5,23 @@ import glob as g
 import MySQLdb as sql
 import aprenentatge
 import decimal as d
+import learndatamani as l
+
 path = './image/*.jpg' 
 tam = len(path) - 5  
 x = g.glob(path)  
-fitxer = raw_input("Introdueix el nom del fitxer del aprenentatge:") 
-if ~os.path.isfile(fitxer): 
+nfile = raw_input("Introdueix el nom del fitxer del aprenentatge:") 
+if not os.path.isfile(nfile): 
 	query = "SELECT count(*) FROM sed2013_task2_dataset_train_tags;" 
 	db = sql.connect(host="localhost", user="root", passwd="root",db="gdsa") 
-	mapren =aprenentatge.aprenentatge(x,tam,db)
+	mapren = aprenentatge.aprenentatge(x,tam,db)
 	db.close()
+	l.serialization(mapren,nfile)
+else : 
+	mapren= l.deserialization(nfile)
+	
 
-#	serializaraprenentatge(mapren,fitxer)
-#else : 
-#	deserializaciomapren(&mapren,fitxer)
-
-
-path = raw_input( "Introdueix el path de la carpeta de imatges a classificar, path relatiu o absolut:(per sortir del programa escrigui exit)\n")
-if path == "exit":
-	break
+path = raw_input( "Introdueix el path de la carpeta de imatges a classificar, path relatiu o absolut \n")
 tam = len(path)
 x = g.glob(path+"/*.jpg")
 for image in x:
