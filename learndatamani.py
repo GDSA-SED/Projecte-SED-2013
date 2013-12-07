48e966172c817db7418518c116c9632283045780
Albert Mosella <albert.mosella@gmail.com>
2013-12-07 10:02:45 +0100

    fitxer canviat per nfile

--- learndatamani.py
+++ learndatamani.py
@@ -1,12 +1,12 @@
 # -*- coding: UTF-8 -*-
 import json
-def serialization(mapren,fitxer):
-	f = open(fitxer, 'w')
+def serialization(mapren,nfile):
+	f = open(nfile, 'w')
 	json.dump(mapren, f, encoding="iso-8859-15")
 	f.close()
 
-def deserialization(fitxer):
-	f = open(fitxer, 'r')
+def deserialization(nfile):
+	f = open(nfile, 'r')
 	mapren=json.load(f,encoding="iso-8859-15")
 	f.close()
 	return mapren
