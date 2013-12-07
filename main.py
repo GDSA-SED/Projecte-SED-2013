e3d1be2619dbfb880922a2a050904afca4c14e11
Albert Mosella <albert.mosella@gmail.com>
2013-12-07 16:46:29 +0100

    millorat

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ import os
 import glob as g
 import MySQLdb as sql
 import aprenentatge
+import clasificador
 import learndatamani as l
 
 path = './image/*.jpg' 
 tam = len(path) - 5  
-x = g.glob(path)  
-nfile = raw_input("Introdueix el nom del fitxer del aprenentatge:") 
+x = g.glob(path) 
+db = sql.connect(host="localhost", user="root", passwd="root",db="gdsa")  
+nfile = raw_input("Introdueix el nom del fitxer del aprenentatge:")
 if not os.path.isfile(nfile): 
 	query = "SELECT count(*) FROM sed2013_task2_dataset_train_tags;" 
-	db = sql.connect(host="localhost", user="root", passwd="root",db="gdsa") 
 	learn = aprenentatge.aprenentatge(x,tam,db)
   	l.serialization(learn,nfile)
 else : 
 	learn= l.deserialization(nfile)
-   
- 
+   	
 path = raw_input( "Introdueix el path de la carpeta de imatges a classificar, path relatiu o absolut \n")
 tam = len(path)
 x = g.glob(path+"/*.jpg")
 
 #clasificació de la imatge
-clasificador(learn,x,db,tam)
+clasificador.clasificador(learn,x,db,tam)
 
 db.close()
