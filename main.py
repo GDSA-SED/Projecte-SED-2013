c802a9a9b8982c608c6c2b4cd0d74217502fadcf
Albert Mosella <albert@albert-Satellite.(none)>
2013-12-10 12:08:47 +0100

    eliminada una query inutil

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import glob as g
 import MySQLdb as sql
 import learning as learn
-import clasificator as classi
+import clasificator as clasi
 import learndatamani as l
 
 path = './image/*.jpg' 
@@ -13,7 +13,6 @@ x = g.glob(path)
 db = sql.connect(host="localhost", user="root", passwd="root",db="gdsa")  
 nfile = raw_input("Introdueix el nom del fitxer del aprenentatge:")
 if not os.path.isfile(nfile): 
-	query = "SELECT count(*) FROM sed2013_task2_dataset_train_tags;" 
 	learn = learn.learning(x,tam,db)
   	l.serialization(learn,nfile)
 else : 
