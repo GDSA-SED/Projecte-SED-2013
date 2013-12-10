faa985016f76b1832089925add1af4a9ea241f42
albertmontes <a-montes@hotmail.com>
2013-12-10 12:33:12 +0100

    Update avaluacio2.py

--- avaluacio2.py
+++ avaluacio2.py
@@ -7,36 +7,36 @@ import math as m
 
 nom = raw_input("Introdueixi el nom del fitxer que conte els resultats de les imatges clasificades (escrigui exit per sortir):")
 if nom != "exit":
-        fitxer = open(nom, 'r') #Obrim el fitxer de dades .txt del clasificador end mode lectura
-        cdata = fitxer.readline() #Llegim el contingut de la primera línia del fitxer
+        fitxer = open(nom, 'r') #Open the results .txt file from clasificator in read mode
+        cdata = fitxer.readline() #Read the firts line content
 
-        #Declaració de les variables d'avaluació a calcular
-        pre = [0,0,0,0,0,0,0,0,0] #precisió per clases
-        pre_tot = 0 #precisió total
-        rec = [0,0,0,0,0,0,0,0,0] #record per clases
-        rec_tot = 0 #record total
-        F_score = [0,0,0,0,0,0,0,0,0] #F-score per clases
-        F_score_tot = 0 #F-score total
-        #Matrius de confusió per clases (posició 0: positius certs, 1: positius falsos, 2: negatius certs, 3: negatius falsos)
+        #Declaration of the avaluation variables
+        pre = [0,0,0,0,0,0,0,0,0] #Precision by clases
+        pre_tot = 0 #Total precision
+        rec = [0,0,0,0,0,0,0,0,0] #Recall by clases
+        rec_tot = 0 #rTotal recall
+        F_score = [0,0,0,0,0,0,0,0,0] #F-score by clases
+        F_score_tot = 0 #Total F-score
+        #Confusion matrix by clases (position 0: true positives, 1: false positives, 2: true negatives, 3: false negatives)
         dict_MC = {"sports":[0,0,0,0], "concert":[0,0,0,0], "exhibition":[0,0,0,0], "protest":[0,0,0,0], "fashion":[0,0,0,0], "conference":[0,0,0,0], "theater_dance":[0,0,0,0], "other":[0,0,0,0], "non_event":[0,0,0,0]}
-	cont_imag = 0 #contador de les imatges classificades (necessari pel NMI)
-	dict_cat = {"sports" :set(), "concert":set(), "exhibition":set(), "protest":set(), "fashion":set(), "conference":set(), "theater_dance":set(), "other":set(), "non_event":set()} #Diccionari amb informació de la veritat terreny (necessari pel NMI)
-	dict_clas = {"sports" : set(), "concert":set(), "exhibition":set(), "protest":set(), "fashion":set(), "conference":set(), "theater_dance":set(), "other":set(), "non_event":set()} #Diccionari amb informació de la clasificació (necessari pel NMI)
+	cont_imag = 0 #Image clasified countc(needed for NMI calculation)
+	dict_cat = {"sports" :set(), "concert":set(), "exhibition":set(), "protest":set(), "fashion":set(), "conference":set(), "theater_dance":set(), "other":set(), "non_event":set()} #Dictionary with ground truth information (needed for NMI calculation)
+	dict_clas = {"sports" : set(), "concert":set(), "exhibition":set(), "protest":set(), "fashion":set(), "conference":set(), "theater_dance":set(), "other":set(), "non_event":set()} #Dictionary with clasification information (needed for NMI calculation)
 
-        #Connexió a la base de dades
+        #Database connection
         db = SQL.connect(host="localhost", user="root", passwd="root",db="gdsa")
-        while cdata != "": #Lectura línia a línia el fitxer .txt fins al final
-		cont_imag += 1 #Actualitzem el contador d'imatges
-                ID = cdata[0 : cdata.find(" ")] #Substring que correspon a la ID de la imatge classificada
-                classe = cdata[cdata.find(" ") + 1 : - 1] #Substring que correspon a la clase de la imatge classificada
+        while cdata != "": #Read of claisfication .txt fileline by line
+		cont_imag += 1 #Image count update
+                ID = cdata[0 : cdata.find(" ")] #ID from clasified image
+                classe = cdata[cdata.find(" ") + 1 : - 1] #Event from clasified image
 		dict_clas[classe].add(ID)
                 cursor = db.cursor()
-                #Consulta a la base de dades la veritat terreny de la imatge a través de la seva ID
+                #Ground truth query of the image with that current ID
                 cursor.execute("SELECT event_type FROM sed2013_task2_dataset_train_gs WHERE document_id =" + "'" + ID + "'")
-                classe_db = cursor.fetchone()[0] #Adquisició de la clase de la imatge de la base de dades
+                classe_db = cursor.fetchone()[0] #Ground truth event adquisition
 		dict_cat[classe_db].add(ID)
 
-                #Càlcul dels positius certs, positius falsos, negatius certs i negatius falsos per a cada matriu de confusió de cada classe
+                #True positives,false positives, true negatives and false negatives calculation for each class confusion matrix
                 if classe == classe_db:
                         dict_MC[classe][0] += 1
                         for i in range(len(dict_MC)):
@@ -51,49 +51,49 @@ if nom != "exit":
                         dict_MC[classe_db][3] += 1
                         dict_MC[classe][1] += 1
 
-                cdata = fitxer.readline() #Lectura de la següent línia
+                cdata = fitxer.readline() #Reading of the next line
         fitxer.close()
 
-        #Càlcul dels paràmetres de precisió, record i F-Score a partir de les matrius de confusió
-        num_div_p = 0 #Número divisori per calcular la mitjana de precisió
-        num_div_r = 0 #Número divisori per calcular la mitjana de record
-        num_div_f = 0 #Número divisori per calcular la mitjana de F-score
+        #Precision, recall and F-score calculation using the confusion matrix data
+        num_div_p = 0 #Divisor number for calculate the average precision
+        num_div_r = 0 #Divisor number for calculate the average recall
+        num_div_f = 0 #Divisor number for calculate the average F-score
         for i in range(len(dict_MC)):
                 d = dict_MC.keys()[i]
                 if dict_MC[d][0] + dict_MC[d][1] != 0:
-                        pre[i] = round(float(dict_MC[d][0]) / (dict_MC[d][0] + dict_MC[d][1]),5) #Precisió per clases (5 decimals precisió)
+                        pre[i] = round(float(dict_MC[d][0]) / (dict_MC[d][0] + dict_MC[d][1]),5) #Precision by clases (5 decimal precision)
                 elif dict_MC[d][1] != 0:
                         pre[i] = 0
                 else:
                         pre[i] = "none"
                 if dict_MC[d][0] + dict_MC[d][3] != 0:
-                        rec[i] = round(float(dict_MC[d][0]) / (dict_MC[d][0] + dict_MC[d][3]),5) #Record per clases (5 decimals precisió)
+                        rec[i] = round(float(dict_MC[d][0]) / (dict_MC[d][0] + dict_MC[d][3]),5) #Recall by clases (5 decimal precision)
                 elif dict_MC[d][3]!= 0:
                         rec[i] = 0
                 else:
                         rec[i] = "none"
                 if pre[i] != "none" and rec[i] != "none":
                         if pre[i] + rec[i] != 0:
-                                F_score[i] = round(2 * pre[i] * rec[i] / (pre[i] + rec[i]),5) #F-Score per clases (5 decimals precisió)
+                                F_score[i] = round(2 * pre[i] * rec[i] / (pre[i] + rec[i]),5) #F-Score by clases (5 decimal precision)
                         else:
                                 F_score[i] = 0
                 else:
                         F_score[i] = "none"
         for i in range(len(dict_MC)):
                 if pre[i] != "none":
-                        pre_tot = pre[i] + pre_tot #Càlcul de la precisió total
+                        pre_tot = pre[i] + pre_tot #Average precision calculation
                         num_div_p += 1
                 if rec[i] != "none":
-                        rec_tot = rec[i] + rec_tot #Càlcul del record total
+                        rec_tot = rec[i] + rec_tot #Average Recall Calculation
                         num_div_r += 1
                 if F_score[i] != "none":
-                        F_score_tot = F_score[i] + F_score_tot #Càlcul de la F-Score total
+                        F_score_tot = F_score[i] + F_score_tot #Average F-score calculation
                         num_div_f += 1
-        pre_tot = round(pre_tot / num_div_p,5) #Precisió total normalitzada (5 decimals precisió)
-        rec_tot = round(rec_tot / num_div_r,5) #Record total normalitzat (5 decimals precisió)
-        F_score_tot = round(F_score_tot / num_div_f,5) #F-Score total normalitzada (5 decimals precisió)
+        pre_tot = round(pre_tot / num_div_p,5) #Normalized average precision (5 decimal precision)
+        rec_tot = round(rec_tot / num_div_r,5) #Normalized average recall (5 decimal precision)
+        F_score_tot = round(F_score_tot / num_div_f,5) #Normalized average F-score (5 decimal precision)
 
-	#Càlcul NMI
+	#NMI calculation
 	den1_nmi = 0.0
 	den2_nmi = 0.0
 	num_nmi = 0.0
@@ -109,9 +109,9 @@ if nom != "exit":
 			num_nmi += (float(len(dict_clas[event_d1] & dict_cat[event]))/float(cont_imag)) * (m.log((float(cont_imag) * float(len(dict_clas[event_d1] & dict_cat[event]))) / (float(len(dict_clas[event_d1])) * float(len(dict_cat[event]))), 2))
 	for event_d2 in dict_cat:
 		den2_nmi += (float(len(dict_cat[event_d2])) / float(cont_imag)) * m.log(float(len(dict_cat[event_d2])) / float(cont_imag),2)
-	nmi = round(num_nmi / (((-1)*den1_nmi + (-1)*den2_nmi) / 2), 5) #NMI (5 decimals precisió)
+	nmi = round(num_nmi / (((-1)*den1_nmi + (-1)*den2_nmi) / 2), 5) #NMI (5 decimal precision)
 	
-        #Creació d'una gràfica amb els resultats obtinguts a l'avaluació
+        #Avaluation results comparison bar graph
 	val_table = [[pre[8],rec[8],F_score[8]], [pre[4],rec[4],F_score[4]], [pre[7],rec[7],F_score[7]], [pre[2],rec[2],F_score[2]], [pre[3],rec[3],F_score[3]], [pre[0],rec[0],F_score[0]], [pre[5],rec[5],F_score[5]], [pre[1],rec[1],F_score[1]], [pre[6],rec[6],F_score[6]], [pre_tot, rec_tot, F_score_tot]]
 	n = np.array(range(10))
         val_p = [0,0,0,0,0,0,0,0,0,0]
@@ -138,7 +138,7 @@ if nom != "exit":
         ax.autoscale(tight=True)
         pl.subplots_adjust(right = 0.85,bottom = 0.35)
 
-	#Creació d'una taula amb els resultats obtinguts a l'avaluació
+	#Avaluation results comparison table
         etiquetas_fil = ('sports', 'concert', 'exhibition', 'protest', 'fashion', 'conference', 'theater_dance', 'other', 'non_event', 'AVERAGE')
         etiquetas_col = ('Precision', 'Recall', 'F-Score','NMI')
         val_table = [[pre[8],rec[8],F_score[8],"-"], [pre[4],rec[4],F_score[4],"-"], [pre[7],rec[7],F_score[7],"-"], [pre[2],rec[2],F_score[2],"-"], [pre[3],rec[3],F_score[3],"-"], [pre[0],rec[0],F_score[0],"-"], [pre[5],rec[5],F_score[5],"-"], [pre[1],rec[1],F_score[1],"-"], [pre[6],rec[6],F_score[6],"-"], [pre_tot, rec_tot, F_score_tot,nmi]]
