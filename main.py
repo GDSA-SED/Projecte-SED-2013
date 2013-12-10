7d9dd724258a932e05acff580cd000efbb4a505a
David <thefackinboss@users.noreply.github.com>
2013-12-10 12:28:02 +0100

    traducció del main

--- main.py
+++ main.py
@@ -7,21 +7,32 @@ import learning as learn
 import clasificator as clasi
 import learndatamani as l
 
-path = './image/*.jpg' 
-tam = len(path) - 5  
-x = g.glob(path) 
-db = sql.connect(host="localhost", user="root", passwd="root",db="gdsa")  
-nfile = raw_input("Introdueix el nom del fitxer del aprenentatge:")
-if not os.path.isfile(nfile): 
-	learn = learn.learning(x,tam,db)
-  	l.serialization(learn,nfile)
-else : 
-	learn= l.deserialization(nfile)
-   	
-path = raw_input( "Introdueix el path de la carpeta de imatges a classificar, path relatiu o absolut \n")
+# Define path and size of the ID of the image
+path = './image/*.jpg'
+tam = len(path) - 5
+
+x = g.glob(path)
+
+#Establish the connection with our mysql database
+db = sql.connect(host="localhost", user="root", passwd="root",db="gdsa")
+
+
+nfile = raw_input("Insert the name of the learning file: ")
+
+#if this learning file doesn't exist we create it
+if not os.path.isfile(nfile):
+        dlearn = learn.learning(x,tam,db)
+        l.serialization(dlearn,nfile)
+else :
+        dlearn= l.deserialization(nfile)
+
+           
+path = raw_input( "Insert the path of the folder of images that you want to clasificate, the path can be ralative or absolut: \n")
 tam = len(path)+1
 x = g.glob(path+"/*.jpg")
-#clasificació de la imatge
-clasi.clasificator(learn,x,db,tam)
 
+#Image clasification
+clasi.clasificator(dlearn,x,db,tam)
+
+#close the database
 db.close()
