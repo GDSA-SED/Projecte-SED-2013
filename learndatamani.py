ff2304b440fed5952cf4b29e08e66768ee597719
AlbertMosella <albert.mosella@gmail.com>
2013-12-11 18:50:28 +0100

    Update learndatamani.py

--- learndatamani.py
+++ learndatamani.py
@@ -4,7 +4,7 @@ def serialization(mapren,nfile):
 	f = open(nfile, 'w')
 	json.dump(mapren, f, encoding="iso-8859-15")
 	f.close()
-
+	
 def deserialization(nfile):
 	f = open(nfile, 'r')
 	mapren=json.load(f,encoding="iso-8859-15")
