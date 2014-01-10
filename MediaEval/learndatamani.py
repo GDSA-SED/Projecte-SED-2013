f284595f0493a3871348e07c0a8b5342acef82ae
albertmontes <a-montes@hotmail.com>
2014-01-10 10:19:31 +0100

    Create learndatamani.py

---
+++ MediaEval/learndatamani.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+import json
+def serialization(mapren,nfile):
+        f = open(nfile, 'w')
+        json.dump(mapren, f, encoding="iso-8859-15")
+        f.close()
+        
+def deserialization(nfile):
+        f = open(nfile, 'r')
+        mapren=json.load(f,encoding="iso-8859-15")
+        f.close()
+        return mapren
